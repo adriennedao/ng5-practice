8179e2b96866301a4555f229cbf7d603ec1839f5
AdrienneD <adriennedao@gmail.com>
2018-06-27 23:26:44 -0400

    added css style rule

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [`
-    p { font-weight: bold }
-    div { color: gray; }
-  `]
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
